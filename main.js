e8bfc35b34b23122311009544e9b4249cbccf150
Antonio Jiménez <marshday5@gmail.com>
2024-03-15 22:51:08 -0600

    object literally

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const jesus = {};
+// objeto literal 
+const jesus = {
+    name: "Antonio",
+    age: 24,
+    cursosAprovados: [
+        "curso de html y css",
+        "curso práctico de html y css",
+    ]
+};
+
+
+console.log(jesus);
