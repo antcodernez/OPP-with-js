4daf43b16011c2e2ca9b2d6ad7e1a4e555ea9afe
Antonio Jiménez <marshday5@gmail.com>
2024-04-01 20:36:23 -0600

    using the class sintax

--- main.js
+++ main.js
@@ -48,5 +48,32 @@ Student.prototype.aprobarCurso = function (newCourse)
 const Juanita = new Student("Juanita", 19, ["hmtl y css", "ciberseguridad"]);
 
 console.log(Juanita);
-
 //Los metodos definidos en el prototipo se pueden observar dentro de la propiedad proto
+console.groupEnd();
+
+//Sintaxis de clases 
+console.groupCollapsed("Clases en JS");
+
+//Recibir parametros en el constructor en forma de objeto usando el patron de diseno RORO, funciona en las clases y en la sintaxis de prototipos(a veces en prototipos)
+
+class Human {
+    constructor({
+        name, lastName = "ALV", age
+    }) 
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+            this.cursosAprobados = [];
+        }
+
+    aprobarCurso(curso) {
+        this.cursosAprobados.push(curso);
+    }
+}
+
+const pedro = new Human({ name: "Pedro", age: 23});
+
+pedro.aprobarCurso("Matemáticas en francés");
+console.log(pedro);
+console.groupEnd();
