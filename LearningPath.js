f5116809844afcacc6fd71011ec4d56c0aadd538
Antonio Jiménez <marshday5@gmail.com>
2024-04-03 23:41:21 -0600

    making the class Learning paths

---
+++ LearningPath.js
@@ -0,0 +1,41 @@
+class learningPath {
+    constructor ({
+        courses = [],
+        name,
+        school = null,
+        teachers = [],
+        added = false,
+    }) {
+        this.courses = courses;
+        this.name = name;
+        this.school = school;
+        this.teachers = teachers;
+        this.added = added;
+        this.numberOfCourses = courses.length;
+    }
+
+    addCourse (course) {
+        this.courses.push(course);
+    }
+
+    removeCourse(courseToRemove) {
+        let indice = this.courses.indexOf(courseToRemove);
+        if (indice > -1) 
+            {
+                this.courses.splice(indice, 1);
+            }
+    }
+    addTeacher (teacher) {
+        this.teachers.push(teacher);
+    }   
+    removeTeacher(teachers) {
+        const newArrayTeachers = this.teachers.filter(teacher => teacher !== teachers);
+        this.teachers = newArrayTeachers;
+    }
+    addRoute () {
+        this.added = true
+    }
+    removeRoute () {
+        this.added = false
+    }
+}
